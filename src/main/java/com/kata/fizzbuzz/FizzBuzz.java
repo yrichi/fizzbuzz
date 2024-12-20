9eb744af80f45681b3c0c27be765f41c0fce3da8
yrichi <yrichi59@gmail.com>
2024-12-20 20:00:10 +0100

    refacto

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -11,15 +11,12 @@ public class FizzBuzz {
             return FIZZ+BUZZ;
         }
 
-        if (number % 3 != 0 && number % 5 != 0) {
-            return String.valueOf(number);
-        }
         if (number % 3 == 0 ) {
             return FIZZ;
         }
         if (number % 5 == 0) {
             return BUZZ;
         }
-        return "";
+          return String.valueOf(number);
     }
 }
